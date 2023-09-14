e3b8317cc9cbb9bc79c212200c3b21c456713363
whynot <anil@panix.com>
2023-09-14 16:15:55 -0400

    Methods

--- code/JavaFundas/src/main/java/ttl/live/shape/Shape.java
+++ code/JavaFundas/src/main/java/ttl/live/shape/Shape.java
@@ -7,22 +7,26 @@ public class Shape {
 
     private final static String defaultColor = "Black";
     private final static int defaultLineThickness = 1;
-    private int x;
-    private int y;
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
     private String color = defaultColor;
     private int lineThickness = defaultLineThickness;
 
-    public Shape(int x, int y) {
-        this(x, y, defaultColor, defaultLineThickness);
+    public Shape(int x1, int y1, int x2, int y2) {
+        this(x1, y1, x2, y2, defaultColor, defaultLineThickness);
 //        this.x = x;
 //        this.y = y;
 
 //        complicatedStuff();
     }
 
-    public Shape(int x, int y, String color, int lineThickness) {
-        this.x = x;
-        this.y = y;
+    public Shape(int x1, int y1, int x2, int y2, String color, int lineThickness) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
         this.color = color;
 //        this.lineThickness = lineThickness;
         setLineThickness(lineThickness);
@@ -37,20 +41,36 @@ public class Shape {
     }
 
 
-    public int getX() {
-        return x;
+    public int getX1() {
+        return x1;
     }
 
-    public void setX(int x) {
-        this.x = x;
+    public void setX1(int x1) {
+        this.x1 = x1;
     }
 
-    public int getY() {
-        return y;
+    public int getY1() {
+        return y1;
     }
 
-    public void setY(int y) {
-        this.y = y;
+    public void setY1(int y1) {
+        this.y1 = y1;
+    }
+
+    public int getX2() {
+        return x2;
+    }
+
+    public void setX2(int x2) {
+        this.x2 = x2;
+    }
+
+    public int getY2() {
+        return y2;
+    }
+
+    public void setY2(int y2) {
+        this.y2 = y2;
     }
 
     public String getColor() {
@@ -69,5 +89,15 @@ public class Shape {
         if(lineThickness > 0) {
             this.lineThickness = lineThickness;
         }
+    }
+
+    public void draw() {
+        System.out.printf("In Shape::draw, x1: %d, y1: %d, x2: %d, y2: %d\n", x1, y1, x2, y2);
+    }
+
+    public double getArea() {
+        int width = this.x2 - this.x1;
+        int height = this.y2 - this.y1;
+        return width * height;
     }
 }
--- code/JavaFundas/src/test/java/ttl/live/shape/TestShapes.java
+++ code/JavaFundas/src/test/java/ttl/live/shape/TestShapes.java
@@ -11,28 +11,44 @@ public class TestShapes {
 
     @Test
     public void testBasicShape() {
-        Shape shape1 = new Shape(10, 10, "Red", -100);
+        Shape shape1 = new Shape(10, 10, 20, 20, "Red", -100);
 //        shape1.setX(10);
 ////        shape1.setY(10);
 //        shape1.setColor("Red");
 //        shape1.setLineThickness(-100);
 
-        assertEquals(10, shape1.getX());
+        assertEquals(10, shape1.getX1());
         assertEquals(1, shape1.getLineThickness());
     }
 
     @Test
     public void testShapeConstructors() {
-        Shape shape1 = new Shape(10, 10, "Red", -100);
+        Shape shape1 = new Shape(10, 10, 20, 20, "Red", -100);
 
-        Shape shape2 = new Shape(10, 10);
+        Shape shape2 = new Shape(10, 10, 20, 20 );
 //        shape1.setX(10);
 ////        shape1.setY(10);
 //        shape1.setColor("Red");
 //        shape1.setLineThickness(-100);
 
-        assertEquals(10, shape2.getX());
+        assertEquals(10, shape2.getX1());
         assertEquals(1, shape2.getLineThickness());
         assertEquals("Black", shape2.getColor());
+    }
+
+
+    @Test
+    public void testBehaviours() {
+        Shape shape1 = new Shape(10, 10, 20, 20);
+
+        Shape shape2 = new Shape(40, 40, 60, 60);
+
+
+        double area = shape1.getArea();
+
+        double area2 = shape2.getArea();
+
+        System.out.println("area: " + area);
+        assertEquals(100.0, area);
     }
 }
